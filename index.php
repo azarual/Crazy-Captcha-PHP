a0a626eb26867c3a904743567db60fbcd7ae678e
blog@igorescobar.com <igor@visie.com.br>
2010-09-22 17:34:04 -0300

    Melhorando visual do captcha e aumentando o suporte para outros sistemas operacionais

--- index.php
+++ index.php
@@ -1,67 +1,63 @@
 <?php
+/**
+ * @author Igor Escobar  (@igorescobar)
+ * 
+ * Crazy-Captcha-PHP - An image security generator 
+ * to deny robot access.
+ *
+ * @source http://github.com/igorescobar/Crazy-Captcha-PHP
+ * 
+ * @original-source by @dgmike at http://github.com/dgmike/captcha
+ */
 
 // desabilitando o cache
-header ("Content-type: image/png");
-header('Pragma: no-cache');
-header('Cache-Control: private, no-cache, proxy-revalidate');
+header ( 'Content-type: image/png' );
+header ( 'Pragma: no-cache');
+header ( 'Cache-Control: private, no-cache, proxy-revalidate');
 
 // é muito mais seguro usar sessoes.
 session_start();
 
-// Cria a imagem GD do captcha
-$im = imagecreatetruecolor ( 150, 70 )
-    or die("Cannot Initialize new GD image stream");
+$captcha_size = array ( 300, 70 );
+$captcha_total_letters = 5;
 
-// Preenche de cinza
-$bg = imagecolorallocate ( $im, 255, 255, 255 );
-imagefill ( $im, 0, 0, $bg );
+
+// Cria a imagem GD do captcha
+$im = imagecreatetruecolor ( $captcha_size[0], $captcha_size[1] ) 
+		or die("Cannot Initialize new GD image stream");
+
+// Preenche o background de branco
+imagefill ( $im, 0, 0, imagecolorallocate ( $im, 255, 255, 255 ) );
 
 // Coloca os chanfros aleatorios na imagem
-for ( $i = 0 ; $i < 1000 ; $i++ ) {
-    $lines = imagecolorallocate ( $im, rand ( 200, 220 ), rand ( 200, 220 ), rand ( 200, 220 ) );
-    $start_x = rand ( 0,150 );
-    $start_y = rand ( 0,70 );
-    $end_x = $start_x + rand ( 0,5 );
-    $end_y = $start_y + rand ( 0,5 );
+for ( $i = 0 ; $i < 300 ; $i++ ) {
+    $lines = imagecolorallocatealpha ( $im, rand ( 0, 255 ), rand ( 0, 225 ), rand ( 0, 225 ) , 80 );
+    $start_x = rand ( 0, $captcha_size[0] );
+    $start_y = rand ( 0, $captcha_size[1] );
+    $end_x = $start_x + rand ( 0, 1000);
+    $end_y = $start_y + rand ( 0, 1000 );
     imageline ( $im, $start_x, $start_y, $end_x, $end_y, $lines );
 }
 
-$logo = imagecreatefrompng ( 'logo.png' );
+// String com as letras que vao aparecer no captcha
+$letters = 'ABCDEFGHJKLMNPRTUVWXYZ2346789';
+$letters = str_split ( $letters );
 
-$alt = rand ( -130, -30 );
-$larg = rand ( -130, -30 );
-
-imagecopy($im, $logo, $larg, $alt, 0, 0, 150 - $larg, 70 - $alt);
-
-// Cria o array com as letras do captcha
-$letters = array_merge ( range ( 'A', 'Z' ) , range ( 2, 9 ) );
-
-// remove as letras que possam confundir o usuário
-unset ( $letters[ array_search ( 'O', $letters ) ] );
-unset ( $letters[ array_search ( 'Q', $letters ) ] );
-unset ( $letters[ array_search ( 'I', $letters ) ] );
-unset ( $letters[ array_search ( '5', $letters ) ] );
-unset ( $letters[ array_search ( 'S', $letters ) ] );
-
-// mistura as letras
-shuffle($letters); 
+shuffle ( $letters ); 
 
 // pega somente 4 posicoes (letras) do array 
-$letters = array_slice($letters, 0, 4);
+$letters = array_slice ( $letters, 0, $captcha_total_letters );
 
 // transforma o array em uma string
-$secure_text = implode('', $letters);
+$secure_text = implode ( '', $letters );
+
+$font = imageloadfont ( './font.gdf' );
 
 // escreve as letras na imagem
-$i = 0;
-foreach ( $letters as $letter):
-    $text_color = imagecolorallocate ( $im, rand ( 0,100 ), rand ( 10,100 ), rand ( 0,100 ) );
-	if ( ! function_exists ( 'imagefttext' ) )
-    	imagestring ( $im, 10, 20 + ( $i * 25 ) + rand ( -5, +5 ), 10 + rand ( 10, 30 ),  $letter, $text_color );
-	else
-    	imagefttext ( $im, 35, rand ( -10, 10 ), 20 + ($i * 30) + rand ( -5, +5 ), 35 + rand ( 10, 30 ),  $text_color, './font.ttf', $letter );
-
-    $i++;
+foreach ( $letters as $key => $letter):
+    	$text_color = imagecolorallocatealpha ( $im, rand ( 0, 100 ), rand ( 10, 100 ), rand ( 0, 100 ), rand ( 30, 60 ) );
+		$captcha_center = (($captcha_size[0] / 2) - ( $captcha_total_letters * 21 ) );
+		imagestring ( $im, $font,  $captcha_center + ( $key * 33 ) , 5 + rand ( 5, 20 ), $letter, $text_color );
 endforeach;
 
 // armazena na sessão o MD5 da imagem que foi gerada
